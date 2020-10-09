79df8c0b2797892ec53be140ec9dcc9193287645
Mjavaidnawab <57194178+Mjavaidnawab@users.noreply.github.com>
2020-10-09 13:05:59 +0400

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,7 @@ function Home() {
     <div >
       <h1> PIAIC Boot Camp 2020 : Class 11 - React Router V6</h1>
       <h2> Student : Muhammad Javaid</h2>
-      <h2> Project 03 : Shoe Store</h2>
+      <h2> Project 03 : Shoe Store </h2>
     </div>
   );
 }
